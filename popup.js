4a1219c0779dc6a2164ab52a8aa53e5d3d5edcde
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2024-08-17 21:53:00 +0530

    boiler plate and skeleton code

---
+++ popup.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // IN THAT WE GOING TO TARGET THE ELEMENT
+
+    document.getElementById("accountList");
+})
+
+// STATE VARIABLE
+let providerURL = '';
+
+let provider;
+let privateKey;
+let address;
+
+// FUNCTION
+function handler() {};
+
+function checkBalance() {};
+
+function getOpenNetwork() {};
+
+function getSelectedNetwork() {};
+
+function setNetwork() {};
+
+function loginUser() {};
+
+function createUser() {};
+
+function openCreate() {};
+
+function login() {};
+
+function logout() {};
+
+function openTransfer() {};
+
+function goBack() {};
+
+function openImport() {};
+
+function importGoBack() {};
+
+function openActivity() {};
+
+function openAssets() {};
+
+function goHomePage() {};
+
+function openImportModel() {};
+
+function closeImportModal() {};
+
+function addToken() {};
+
+function addAccount() {};
+
+function myFunction() {};
+
+function copyAddress() {};
+
+function changeAccount() {};
